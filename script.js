9dbddcaeec810fdd9787f084c5c6bf2320c621e8
Andre Vanha <andre@vanhas.com>
2025-01-10 16:49:15 -0700

    Update to new Google library

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+function onSignIn(credential) {
+    console.log('Sub: ' + credential.sub); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + credential.given_name + "" + credential.family_name);
+    console.log('Picture: ' + credential.picture);
+    console.log('Email: ' + credential.email); // This is null if the 'email' scope is not present.
   }
