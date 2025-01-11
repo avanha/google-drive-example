0757ef2615d88e554e20bb66f88248dd313094de
Andre Vanha <andre@vanhas.com>
2025-01-10 17:00:50 -0700

    Decode JWT

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function onSignIn(credential) {
-    console.log('Sub: ' + credential.sub); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + credential.given_name + "" + credential.family_name);
-    console.log('Picture: ' + credential.picture);
-    console.log('Email: ' + credential.email); // This is null if the 'email' scope is not present.
+
+ function decodeJwtResponse(token) {
+   let base64Url = token.split('.')[1];
+   let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+   let jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+   }).join(''));
+
+   return JSON.parse(jsonPayload);
+ }
+
+function onSignIn(response) {
+    const payload = decodeJwtResponse(response.credential)
+    console.log('Sub: ' + payload.sub); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + payload.name);
+    console.log('Picture: ' + payload.picture);
+    console.log('Email: ' + payload.email); // This is null if the 'email' scope is not present.
   }
+
